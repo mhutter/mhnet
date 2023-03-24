d09b77474a6e5a722deb84f9595a62c0ae6ac13c
Manuel Hutter <manuel@hutter.io>
2023-03-24 07:06:37 +0100

    Clean up k0s stuff
    Signed-off-by: Manuel Hutter <manuel@hutter.io>

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -19,16 +19,6 @@ variable "ssh_key" {
   default = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILRFlkyW0MXxYjA1HUzJ18nlTLtXOHKV0rVJD/46v7Sb tera2023"
 }
 
-variable "controller_count" {
-  type    = number
-  default = 1
-}
-
-variable "worker_count" {
-  type    = number
-  default = 3
-}
-
 variable "ip_range" {
   type    = string
   default = "10.42.0.0/24"
@@ -37,9 +27,7 @@ variable "ip_range" {
 variable "ip_offsets" {
   type = map(number)
   default = {
-    "lb"         = 2
-    "bastion"    = 10
-    "worker"     = 100
-    "controller" = 200
+    "bastion" = 10
+    "ares"    = 20
   }
 }
